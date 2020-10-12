f70cdd4941c53ec1e4c00261cd78c9963c6eabf8
hopeshaventn <72173520+hopeshaventn@users.noreply.github.com>
2020-10-11 21:09:32 -0400

    Add files via upload

--- contactForm.php
+++ contactForm.php
@@ -3,13 +3,13 @@
     $vistor_email = $_POST['email'];
     #$message = $_POST['message'];
 
-    $email_from = 'zachbianucci@gmail.com';
+    $email_from = 'feletinucci@gmail.com';
     $email_subject = "I want to stay in touch!";
     $email_body = "Hey Hope's Haven! My email is $vistor_email and I would like for you to reach out to me!";
     $email_body = wordwrap($message, 70, "\r\n");
     $to = "zachbianucci@gmail.com";
 
-    $headers = 'From: zachbianucci@gmail.com' . "\r\n".
+    $headers = 'From: '$email_from . "\r\n".
         'Reply-To: '$vistor_email."\r\n".
         'X-Mailer: PHP'.phpversion();
 
